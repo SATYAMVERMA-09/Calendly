7f990666abb90174e15210a8154c5e9dfbde29c7
Satyam Verma <satyamverma0907@gmail.com>
2026-07-03 21:54:43 +0530

    Added routeNotFound Middleware

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import express, { Express } from 'express';
 import { userRouter } from './routers/user.router.js';
 import { errorHandler } from './middleware/error-handler.js';
+import { routeNotFound } from './middleware/route-not-found.js';
 
 const app: Express = express();
 
@@ -8,9 +9,15 @@ app.use(express.json());
 app.use(express.text());
 app.use(express.urlencoded());
 
- 
+app.get('/health',(_req,res)=>{
+        res.json({
+            status: "ok!",
+            timestamp: new Date().toISOString()
+        })
+})
 
 app.use('/api/users', userRouter);
 
+app.use(routeNotFound);
 app.use(errorHandler);
 export { app };
